9d292d301d419c4500ef8bfb1ea808b314833034
Udo Gayer <ugayer@ikp.tu-darmstadt.de>
2017-02-02 17:15:51 +0100

    Event generation: Angular distribution
    - Implemented Monte-Carlo angular distribution generator.
    - Implemented angular distributions for some two-step cascades that are
      important in NRF
    - Added an optional check of the event generator (does not work yet)

--- include/AngularDistributionGenerator.hh
+++ include/AngularDistributionGenerator.hh
@@ -3,6 +3,7 @@
 
 #include "G4ParticleGun.hh"
 #include "G4ThreeVector.hh"
+#include "G4Navigator.hh"
 #include "G4VUserPrimaryGeneratorAction.hh"
 
 class AngularDistributionGenerator : public G4VUserPrimaryGeneratorAction {
@@ -11,9 +12,29 @@ class AngularDistributionGenerator : public G4VUserPrimaryGeneratorAction {
 	~AngularDistributionGenerator();
 
 	void GeneratePrimaries(G4Event *anEvent);
+	G4ThreeVector AngularDistributionRandomDirection(G4double *states, G4int nstates, G4double* mixing_ratios);
 
   private:
 	G4ParticleGun *particleGun;
+
+	G4String particleName;
+	G4String Source_PV_Name;
+	G4double particleEnergy;
+
+	G4double source_x;
+	G4double source_y;
+	G4double source_z;
+
+	G4double range_x;
+	G4double range_y;
+	G4double range_z;
+
+	G4String pv;
+	G4double random_x;
+	G4double random_y;
+	G4double random_z;
+
+	G4Navigator* navi;
 };
 
 #endif
