89d529e2682954d5cd48b2c5ee928ec25c04eac1
Oliver Papst <opapst@ikp.tu-darmstadt.de>
2017-11-09 11:14:36 +0100

    loop scripts take parameters

--- OutputProcessing/loopGetHistogram.sh
+++ OutputProcessing/loopGetHistogram.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# Executes the getHistogram script in a loop. The code below would do
-# ./getHistogram utr utr0 .root utr0.root
-# ./getHistogram utr utr1 .root utr1.root
-# ...
+# loopGetHistogram.sh <start> <end> <histname> <prefix>
+# Executes the getHistogram script in a loop.
 
-for i in `seq 0 23`
+if [ "$#" -ne 4 ]; then
+    echo "Illegal number of parameters"
+    echo "usage: $0 <start> <end> <histname> <prefix>"
+    echo "This would do"
+    echo "./getHistogram <histname> <prefix><start>_ .root <prefix><start>.root"
+    echo "..."
+    echo "./getHistogram <histname> <prefix><end>_ .root   <prefix><end>.root"
+    exit 1
+fi
+
+for i in `seq $1 $2`
 do
-		./getHistogram utr utr"$i"_ .root utr"$i".root 
+    echo ./getHistogram $3 $4"$i"_ .root $4"$i".root
+    #./getHistogram $3 $4"$i"_ .root $4"$i".root
 done
