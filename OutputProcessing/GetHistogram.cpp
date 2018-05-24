f3cad24b1cbc48fe3cb900e4752bb7a3bd756302
Udo Gayer <gayer.udo@gmail.com>
2018-05-24 08:52:50 +0200

    Adapted getHistogram script

--- OutputProcessing/GetHistogram.cpp
+++ OutputProcessing/GetHistogram.cpp
@@ -112,38 +112,41 @@ if(args.verbose){
 
 	// Create histograms
 	
-	const Int_t nhistograms = 9;
+	const Int_t nhistograms = 13;
 
 	TH1* hist[nhistograms];
 
-	hist[0] = new TH1F("h1", "Energy Deposition in HPGe1 (only photons)", 10000, 0.0005, 10.0005);
-	hist[1] = new TH1F("h2", "Energy Deposition in HPGe2 (only photons)", 10000, 0.0005, 10.0005);
-	hist[2] = new TH1F("h3", "Energy Deposition in HPGe3 (only photons)", 10000, 0.0005, 10.0005);
-	hist[3] = new TH1F("h4", "Energy Deposition in HPGe4 (only photons)", 10000, 0.0005, 10.0005);
-	hist[4] = new TH1F("h5", "Energy Deposition in ZeroDegree (only photons)", 10000, 0.0005, 10.0005);
-	hist[5] = new TH1F("h6", "Energy Deposition in HPGe6 (only photons)", 10000, 0.0005, 10.0005);
-	hist[6] = new TH1F("h7", "Energy Deposition in HPGe7 (only photons)", 10000, 0.0005, 10.0005);
-	hist[7] = new TH1F("h8", "Energy Deposition in HPGe8 (only photons)", 10000, 0.0005, 10.0005);
-	hist[8] = new TH1F("h9", "Energy Deposition in HPGe9 (only photons)", 10000, 0.0005, 10.0005);
+	hist[0] = new TH1F("hpge0", "Energy Deposition in ZeroDegree", 12000, 0.0005, 12.0005);
+	hist[1] = new TH1F("hpge1", "Energy Deposition in HPGe1", 12000, 0.0005, 12.0005);
+	hist[2] = new TH1F("hpge2", "Energy Deposition in HPGe2", 12000, 0.0005, 12.0005);
+	hist[3] = new TH1F("hpge3", "Energy Deposition in HPGe3", 12000, 0.0005, 12.0005);
+	hist[4] = new TH1F("hpge4", "Energy Deposition in HPGe4", 12000, 0.0005, 12.0005);
+	hist[5] = new TH1F("labr1", "Energy Deposition in LaBr1", 12000, 0.0005, 12.0005);
+	hist[6] = new TH1F("labr2", "Energy Deposition in LaBr2", 12000, 0.0005, 12.0005);
+	hist[7] = new TH1F("labr3", "Energy Deposition in LaBr3", 12000, 0.0005, 12.0005);
+	hist[8] = new TH1F("labr4", "Energy Deposition in LaBr4", 12000, 0.0005, 12.0005);
+	hist[9] = new TH1F("hpge9", "Energy Deposition in HPGe9", 12000, 0.0005, 12.0005);
+	hist[10] = new TH1F("hpge10", "Energy Deposition in HPGe10", 12000, 0.0005, 12.0005);
+	hist[11] = new TH1F("hpge11", "Energy Deposition in HPGe11", 12000, 0.0005, 12.0005);
+	hist[12] = new TH1F("hpge12", "Energy Deposition in HPGe12", 12000, 0.0005, 12.0005);
 
 	UInt_t multiplicity_counter[nhistograms] = {0};
 
 	// Fill histogram from TBranch in TChain with user-defined conditions
 	// Define variables and read their values from the tree using the GetEntry() method
-	Double_t Edep, Volume, Particle;
+	Double_t Edep, Volume;
 	Double_t Edep_hist[nhistograms] = {0.};
 
 
 	utr.SetBranchAddress("edep", &Edep);
-	utr.SetBranchAddress("particle", &Particle);
 	utr.SetBranchAddress("volume", &Volume);
 
 	for(int i = 0; i < utr.GetEntries(); i++){
 		utr.GetEntry(i);
 
-		if(Edep > 0. && Particle == 22){
+		if(Edep > 0.){
 			
-			if(Volume == 1){
+			if(Volume == 0){
 				Edep_hist[0] += Edep;
 				++multiplicity_counter[0];
 				if(multiplicity_counter[0] == args.multiplicity){
@@ -152,7 +155,7 @@ if(args.verbose){
 					multiplicity_counter[0] = 0;
 				}
 			}
-			if(Volume == 2){
+			if(Volume == 1){
 				Edep_hist[1] += Edep;
 				++multiplicity_counter[1];
 				if(multiplicity_counter[1] == args.multiplicity){
@@ -161,7 +164,7 @@ if(args.verbose){
 					multiplicity_counter[1] = 0;
 				}
 			}
-			if(Volume == 3){
+			if(Volume == 2){
 				Edep_hist[2] += Edep;
 				++multiplicity_counter[2];
 				if(multiplicity_counter[2] == args.multiplicity){
@@ -170,7 +173,7 @@ if(args.verbose){
 					multiplicity_counter[2] = 0;
 				}
 			}
-			if(Volume == 4){
+			if(Volume == 3){
 				Edep_hist[3] += Edep;
 				++multiplicity_counter[3];
 				if(multiplicity_counter[3] == args.multiplicity){
@@ -179,7 +182,7 @@ if(args.verbose){
 					multiplicity_counter[3] = 0;
 				}
 			}
-			if(Volume == 5){
+			if(Volume == 4){
 				Edep_hist[4] += Edep;
 				++multiplicity_counter[4];
 				if(multiplicity_counter[4] == args.multiplicity){
@@ -188,7 +191,7 @@ if(args.verbose){
 					multiplicity_counter[4] = 0;
 				}
 			}
-			if(Volume == 6){
+			if(Volume == 5){
 				Edep_hist[5] += Edep;
 				++multiplicity_counter[5];
 				if(multiplicity_counter[5] == args.multiplicity){
@@ -197,7 +200,7 @@ if(args.verbose){
 					multiplicity_counter[5] = 0;
 				}
 			}
-			if(Volume == 7){
+			if(Volume == 6){
 				Edep_hist[6] += Edep;
 				++multiplicity_counter[6];
 				if(multiplicity_counter[6] == args.multiplicity){
@@ -206,7 +209,7 @@ if(args.verbose){
 					multiplicity_counter[6] = 0;
 				}
 			}
-			if(Volume == 8){
+			if(Volume == 7){
 				Edep_hist[7] += Edep;
 				++multiplicity_counter[7];
 				if(multiplicity_counter[7] == args.multiplicity){
@@ -215,13 +218,49 @@ if(args.verbose){
 					multiplicity_counter[7] = 0;
 				}
 			}
-			if(Volume == 9){
+			if(Volume == 8){
 				Edep_hist[8] += Edep;
 				++multiplicity_counter[8];
 				if(multiplicity_counter[8] == args.multiplicity){
 					hist[8]->Fill(Edep_hist[8]);
 					Edep_hist[8] = 0.;
 					multiplicity_counter[8] = 0;
+				}
+			}
+			if(Volume == 9){
+				Edep_hist[9] += Edep;
+				++multiplicity_counter[9];
+				if(multiplicity_counter[9] == args.multiplicity){
+					hist[9]->Fill(Edep_hist[9]);
+					Edep_hist[9] = 0.;
+					multiplicity_counter[9] = 0;
+				}
+			}
+			if(Volume == 10){
+				Edep_hist[10] += Edep;
+				++multiplicity_counter[10];
+				if(multiplicity_counter[10] == args.multiplicity){
+					hist[10]->Fill(Edep_hist[10]);
+					Edep_hist[10] = 0.;
+					multiplicity_counter[10] = 0;
+				}
+			}
+			if(Volume == 11){
+				Edep_hist[11] += Edep;
+				++multiplicity_counter[11];
+				if(multiplicity_counter[11] == args.multiplicity){
+					hist[11]->Fill(Edep_hist[11]);
+					Edep_hist[11] = 0.;
+					multiplicity_counter[11] = 0;
+				}
+			}
+			if(Volume == 12){
+				Edep_hist[12] += Edep;
+				++multiplicity_counter[12];
+				if(multiplicity_counter[12] == args.multiplicity){
+					hist[12]->Fill(Edep_hist[12]);
+					Edep_hist[12] = 0.;
+					multiplicity_counter[12] = 0;
 				}
 			}
 		}
