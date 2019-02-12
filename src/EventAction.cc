e4a5efa98f0efca46d8f65ffb463276accf19af0
Marcel Schilling <mschlling@ikp.tu-darmstadt.de>
2019-02-12 12:06:29 +0100

    fixed output jumping around cause of lacking decimal positions

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -54,9 +54,9 @@ void EventAction::EndOfEventAction(const G4Event *event) {
 		int hours 	= (ElapsedTime-days*(24*3600))/3600;
 		int minutes 	= (ElapsedTime-days*(24*3600)-hours*3600)/60;
 		int sec 	= (ElapsedTime-days*(24*3600)-hours*3600-minutes*60);
-
+		float percent = (float)((float)eID/(float)NbEvents*100.);
 		G4cout	<< "Progress: ["<<setw(16)<<eID<<"/"<<NbEvents<<"]  "
-				<<setw(4)<<(float)((float)eID/(float)NbEvents*100.)<<" %"
+				<<setw(4)<<std::setprecision(2)<<std::fixed<<percent<<" %"
 				<<"\tRunning time: "<< setw(3)<<days<<"d "<<setw(2)<<hours<<"h "<<setw(3)<<minutes<<"mn "<<setw(3)<<sec<<"s   "
 				<< G4endl;
         }
