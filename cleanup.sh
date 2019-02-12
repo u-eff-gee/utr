808b295b12fd926c3f94c54956b03988c88d127a
Marcel Schilling <mschlling@ikp.tu-darmstadt.de>
2019-02-12 10:45:36 +0100

    changed cleanup.sh to print results and removed bugs

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if [ -e "master.root" ]; then
+	for i in *.root
+	do
+		if [ -e $i ]; then 
+		rm $i
+		fi
+	done
+	echo "All ROOT files removed."
+else
+	echo "No ROOT files found."
+fi
 
-rm *.root
