65de8c3e843112a66319f6eaa589ef9b657a76d2
Marcel Schilling <mschlling@ikp.tu-darmstadt.de>
2019-02-14 14:11:05 +0100

    changed checking procedure for root files

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-if [ -e "master.root" ]; then
-	for i in *.root
-	do
-		if [ -e $i ]; then 
-		rm $i
-		fi
-	done
-	echo "All ROOT files removed."
+b=false
+for i in *
+ do
+ 	j=$(echo $i | tail -c 6)
+ 	k=$(echo $i | head -c 3)
+ 	if [ ".root" == "${j}" ];then
+ 		b=true
+ 		if [ "utr" == "${k}" ];then
+ 			rm $i
+ 		fi
+ 		if [ -e  "master.root" ];then
+ 			rm master.root
+ 		fi
+ 	fi
+ done
+if [ "$b" == "true" ];then
+	echo "All ROOT files from simulation removed."
 else
-	echo "No ROOT files found."
+	echo "No ROOT files from simulation found."
 fi
-
