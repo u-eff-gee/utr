d84780296c2779f802cc9ae51a9b337a73592fcf
uga-uga <gayer.udo@gmail.com>
2019-02-15 08:59:54 +0100

    More general 'safe' cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -3,19 +3,13 @@ b=false
 for i in *
  do
  	j=$(echo $i | tail -c 6)
- 	k=$(echo $i | head -c 3)
  	if [ ".root" == "${j}" ];then
  		b=true
- 		if [ "utr" == "${k}" ];then
- 			rm $i
- 		fi
- 		if [ -e  "master.root" ];then
- 			rm master.root
- 		fi
+		rm $i
  	fi
  done
 if [ "$b" == "true" ];then
-	echo "All ROOT files from simulation removed."
+	echo "All ROOT files removed."
 else
-	echo "No ROOT files from simulation found."
+	echo "No ROOT files found."
 fi
