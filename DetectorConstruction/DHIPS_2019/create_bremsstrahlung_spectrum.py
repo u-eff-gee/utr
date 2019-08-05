ddc72f55da70726ce25b6237e8da92bdc96311c7
Udo Gayer <gayer.udo@gmail.com>
2019-08-05 13:55:18 +0200

    Completed usage instructions for bremsstrahlung macro creator

--- DetectorConstruction/DHIPS_2019/create_bremsstrahlung_spectrum.py
+++ DetectorConstruction/DHIPS_2019/create_bremsstrahlung_spectrum.py
@@ -4,11 +4,11 @@ from numpy import linspace, max, zeros
 
 from schiff import schiff
 
-parser = argparse.ArgumentParser(description='Create an input histogram for Geant4 macros to control the G4GeneralParticleSource which contains the energy spectrum of bremsstrahlung. A model for bremsstrahlung created by an electron beam impinging on a thin target by L.I. Schiff is used.')
+parser = argparse.ArgumentParser(description='Create an input histogram for the G4GeneralParticleSource which contains the energy spectrum of bremsstrahlung. A model for bremsstrahlung created by an electron beam impinging on a thin target by L.I. Schiff is used.')
 parser.add_argument('e0', metavar='E0', type=float, help='Initial energy of the electron beam')
 parser.add_argument('z', metavar='Z', type=int, help='Proton number of the target')
 parser.add_argument('emax', metavar='EMAX', type=float, help='Maximum energy of the histogram')
-parser.add_argument('--nbins', metavar='NBINS', type=int, default=1024, help='Number of bins of the spectrum. Note that a histogram for the ')
+parser.add_argument('--nbins', metavar='NBINS', type=int, default=1024, help='Number of bins of the spectrum. Note that a histogram for the G4GeneralParticleSource may have a maximum of 1024 bins.')
 
 args = parser.parse_args()
 
