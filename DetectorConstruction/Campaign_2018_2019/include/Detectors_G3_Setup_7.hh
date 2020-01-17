63e704f72f500802cd6f26645beaec2d41f4b306
Oliver Papst <opapst@ikp.tu-darmstadt.de>
2020-01-17 09:06:28 +0100

    2018/2019 Gamma3 Setup 7 (same as Setup 10)

---
+++ DetectorConstruction/Campaign_2018_2019/include/Detectors_G3_Setup_7.hh
@@ -0,0 +1,43 @@
+/*
+utr - Geant4 simulation of the UTR at HIGS
+Copyright (C) 2017 the developing team (see README.md)
+
+This file is part of utr.
+
+utr is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+utr is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with utr.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * Setup 10 for G3 was used between run 409 and 489.
+ * In between, for runs 425 to 464, the 2nd setup was
+ * modified to Setup 11, but the G3 setup was not changed.
+ */
+
+#ifndef DETECTORS_G3_SETUP_7_HH
+#define DETECTORS_G3_SETUP_7_HH 1
+
+#include "G4LogicalVolume.hh"
+
+class Detectors_G3_Setup_7{
+public:
+	Detectors_G3_Setup_7(G4LogicalVolume *World_Log);
+	~Detectors_G3_Setup_7(){};
+
+	void Construct(G4ThreeVector global_coordinates);
+
+private:
+	G4LogicalVolume *World_Logical;
+};
+
+#endif
