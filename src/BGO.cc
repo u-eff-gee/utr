7f0621eecff74426cdc7cf4fac01afa9c201787c
Oliver Papst <opapst@ikp.tu-darmstadt.de>
2020-07-29 20:43:37 +0200

    BGO dimensions as static members

--- src/BGO.cc
+++ src/BGO.cc
@@ -15,7 +15,6 @@
 #include "G4Material.hh"
 #include "G4VisAttributes.hh"
 
-#include "G4SystemOfUnits.hh"
 #include "G4NistManager.hh"
 
 #include "NamedColors.hh"
@@ -25,31 +24,12 @@ BGO::BGO(G4LogicalVolume *World_Log, G4String name)
 	World_Logical = World_Log;
 	bgo_name = name;
 
-	G4double caseOut_z[] = {0., 0., 86.*mm, 172.*mm, 172.*mm, 192.*mm, 192.*mm, 193.*mm, 202.*mm, 202.*mm, 42.*mm, 42.*mm};
-	G4double caseOut_r[] = {25.*mm, 62.*mm, 95.*mm, 95.*mm, 115.*mm, 115.*mm, 92.*mm, 92.*mm, 58.5*mm, 50.5*mm, 50.5*mm, 37.44*mm};
-	G4double caseFilled_z[] = {0., 0., 86.*mm, 202.*mm, 202.*mm};
-	G4double caseFilled_r[] = {0., 62.*mm, 95.*mm, 95.*mm, 0.};
-	G4double caseIn_z[] = {8.*mm, 8.*mm, 86.*mm, 179.*mm, 190.*mm, 41.2*mm, 41.2*mm};
-	G4double caseIn_r[] = {27.5*mm, 62.45*mm, 92.*mm, 92.*mm,51.5*mm, 51.3*mm, 37.28*mm};
-	G4double bgo_z[] = {10.*mm, 10.*mm, 70.*mm, 170.*mm, 185.*mm, 40.*mm, 40.*mm};
-	G4double bgo_r[] = {29.2*mm, 60.4*mm, 82.3*mm, 82.3*mm, 52.3*mm, 52.3*mm, 37.*mm};
-	
-	G4double caseGap_z[] = {9.*mm, 9.*mm, 87.*mm, 178.*mm, 189.*mm, 41.5*mm, 41.5*mm};
-	G4double caseGap_r[] = {27.6*mm, 62.4*mm, 91.5*mm, 91.5*mm,51.7*mm, 51.5*mm, 37.38*mm};
-
-	// Convert BGO radius from midpoint to corner
-	for (size_t i = 0; i < sizeof(bgo_r)/sizeof(bgo_r[0]); ++i) {
-		bgo_r[i] = bgo_r[i] / cos(22.5 * deg);
-	}
-	
 	// FIXME: The BGO is further abraded on its edges, but the schematics
 	// don’t indicate how much abrasion there is. The current implementiation
 	// should be sufficient for simulating the HPGe detector (the center of
 	// the tip of the BGO is correct), but the efficiency of the BGO cannot
 	// be determined correctly. The following is a best effort that is better
 	// than doing nothing but probably does not reflect the reality.
-	G4double bgo_abrased_z[] = {10.*mm, 10.*mm, 185.*mm, 185.*mm};
-	G4double bgo_abrased_r[] = {0.*mm, 61.17*mm, 128.34*mm, 0.*mm};
 
 	length = caseOut_z[9];
 	radius = caseOut_z[5];
@@ -59,16 +39,16 @@ BGO::BGO(G4LogicalVolume *World_Log, G4String name)
 	G4Material *Al = man->FindOrBuildMaterial("G4_Al");
 	G4Material *bgo = man->FindOrBuildMaterial("G4_BGO");
 
-	caseFilled_Solid = new G4Polycone(bgo_name + "_caseFilled_Solid", 0., 360.* deg, sizeof(caseFilled_z)/sizeof(caseFilled_z[0]), caseFilled_r, caseFilled_z);
-	auto* caseOut_Solid = new G4GenericPolycone(bgo_name + "_caseOut_Solid", 0., 360. * deg, sizeof(caseOut_z)/sizeof(caseOut_z[0]), caseOut_r, caseOut_z);
-	auto* caseIn_Solid = new G4GenericPolycone(bgo_name + "_caseIn_Solid", 0., 360. * deg, sizeof(caseIn_z)/sizeof(caseIn_z[0]), caseIn_r, caseIn_z);
-	auto* caseGap_Solid = new G4GenericPolycone(bgo_name + "_caseGap_Solid", 0., 360. * deg, sizeof(caseGap_z)/sizeof(caseGap_z[0]), caseGap_r, caseGap_z);
+	caseFilled_Solid = new G4Polycone(bgo_name + "_caseFilled_Solid", 0., 360.* deg, caseFilled_z.size(), caseFilled_r.data(), caseFilled_z.data());
+	auto* caseOut_Solid = new G4GenericPolycone(bgo_name + "_caseOut_Solid", 0., 360. * deg, caseOut_z.size(), caseOut_r.data(), caseOut_z.data());
+	auto* caseIn_Solid = new G4GenericPolycone(bgo_name + "_caseIn_Solid", 0., 360. * deg, caseIn_z.size(), caseIn_r.data(), caseIn_z.data());
+	auto* caseGap_Solid = new G4GenericPolycone(bgo_name + "_caseGap_Solid", 0., 360. * deg, caseGap_z.size(), caseGap_r.data(), caseGap_z.data());
 	auto* bgo_case_Solid = new G4SubtractionSolid(bgo_name + "_case_Solid", caseOut_Solid, caseIn_Solid, 0, G4ThreeVector(0, 0, 0));
 	bgo_case_Logical = new G4LogicalVolume(bgo_case_Solid, Al, bgo_name + "_case_Logical");
 	bgo_case_Logical->SetVisAttributes(magenta);
 
-	auto* bgo_continuous_Solid = new G4Polyhedra(bgo_name + "_continuous_Solid", 0., 360. * deg, 8, sizeof(bgo_z)/sizeof(bgo_z[0]), bgo_r, bgo_z);
-	auto* bgo_abrased_Solid = new G4Polyhedra(bgo_name + "_abrased_Solid", 22.5*deg, 360. * deg, 8, sizeof(bgo_abrased_z)/sizeof(bgo_abrased_z[0]), bgo_abrased_r, bgo_abrased_z);
+	auto* bgo_continuous_Solid = new G4Polyhedra(bgo_name + "_continuous_Solid", 0., 360. * deg, 8, bgo_z.size(), bgo_r.data(), bgo_z.data());
+	auto* bgo_abrased_Solid = new G4Polyhedra(bgo_name + "_abrased_Solid", 22.5*deg, 360. * deg, 8, bgo_abrased_z.size(), bgo_abrased_r.data(), bgo_abrased_z.data());
 	auto* bgo_cut = new G4Tubs("bgo_cut_Solid", 0, radius, length, 0., 45.*deg);
 	auto* bgo_crystal_unfit1_Solid = new G4IntersectionSolid(bgo_name + "_crystal_unfit1_Solid", bgo_continuous_Solid, bgo_cut, 0, G4ThreeVector(.5*cos(22.5*deg), 0.5*sin(22.5*deg), 0.));
 	auto* bgo_crystal_unfit2_Solid = new G4IntersectionSolid(bgo_name + "_crystal_unfit2_Solid", bgo_crystal_unfit1_Solid, bgo_abrased_Solid, 0, G4ThreeVector());
